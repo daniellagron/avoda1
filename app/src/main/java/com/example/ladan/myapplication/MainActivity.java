dbed53c2f2535450b7cba22944f5bdeabc287204
daniellagron <dl8777@bs.amalnet.k12.il>
2018-12-27 22:48:20 +0200

    avoda1

---
+++ app/src/main/java/com/example/ladan/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ladan.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.btn1);
+    }
+
+    public void yeah(View view) {
+        btn1.setText("oh yeah,i`ve been clicked");
+    }
+}
